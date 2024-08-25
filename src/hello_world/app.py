c111849bcd2b1981185ad5f246e3a2ddcbcca5ce
Tim Pugh <nwtpugh@gmail.com>
2024-08-25 13:58:02 -0700

    Add CRUD API Skeleton

--- src/hello_world/app.py
+++ src/hello_world/app.py
@@ -1,7 +1,4 @@
 import json
-#import boto3
-
-# import requests
 
 
 def lambda_handler(event, context):
@@ -26,18 +23,9 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }
