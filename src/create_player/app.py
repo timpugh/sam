c8bdbd71d9a55c1d473c9397f3ba251400d219b0
Tim Pugh <nwtpugh@gmail.com>
2024-09-01 08:45:54 -0700

    add create_player API

--- src/create_player/app.py
+++ src/create_player/app.py
@@ -1,12 +1,29 @@
+import boto3
 import json
-# import boto3
+import os
+import uuid
 
+# Get the DynamoDB resource
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ["TABLE"])
 
+# Function to create a new player
 def lambda_handler(event, context):
+    # Get the player data from the event
+    player_data = event['player']
 
+    # Generate a unique UUID for the player
+    player_data['uuid'] = str(uuid.uuid4())
+
+    # Put the player data into the DynamoDB table
+    response = table.put_item(
+        Item=player_data
+    )
+
+    # Return the response
     return {
-        "statusCode": 200,
+        'statusCode': 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": f"Player {player_data['player_name']} created successfully.",
         }),
     }
