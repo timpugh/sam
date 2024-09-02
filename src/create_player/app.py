27f35ff6eea490997956fd3b444e1f38ee305816
Tim Pugh <nwtpugh@gmail.com>
2024-09-01 20:09:25 -0700

    Add exception handling for create_player API

--- src/create_player/app.py
+++ src/create_player/app.py
@@ -7,6 +7,7 @@ import uuid
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ["TABLE"])
 
+
 # Function to create a new player
 def lambda_handler(event, context):
     # Get the player data from the event
@@ -17,14 +18,25 @@ def lambda_handler(event, context):
     player_data['uuid'] = str(uuid.uuid4())
 
     # Put the player data into the DynamoDB table
-    response = table.put_item(
-        Item=player_data
-    )
+    try:
+        response = table.put_item(
+            Item=player_data
+        )
 
-    # Return the response
-    return {
-        'statusCode': 200,
-        "body": json.dumps({
-            "message": f"Player {player_data['player_name']} created successfully.",
-        }),
-    }
+        # Return the response
+        return {
+            'statusCode': 200,
+            "body": json.dumps({
+                "message": f"Player {player_data['player_name']} created successfully.",
+            }),
+        }
+
+    except Exception as e:
+        player_name = player_data['player_name']
+
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': f"Error adding player {player_name}: {str(e)}"
+            })
+        }
