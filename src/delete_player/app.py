154be4caa51eab3d209db6cd1f34a1c6d45d8169
Tim Pugh <nwtpugh@gmail.com>
2024-09-01 21:05:26 -0700

    Add delete_player API

--- src/delete_player/app.py
+++ src/delete_player/app.py
@@ -1,12 +1,40 @@
+import boto3
 import json
-# import boto3
+import os
+from urllib.parse import unquote
+
+# Get the DynamoDB resource
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ["TABLE"])
 
 
+# Function to delete a player
 def lambda_handler(event, context):
+    # Get the player data from the event
+    player_name =  unquote(event['pathParameters']['id'])
+    season = event['pathParameters']['date']
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        }),
+    # Construct the key dictionary
+    key = {
+        'player_name': player_name,
+        'season': season
     }
+
+    # Delete the player data in the DynamoDB table
+    try:
+        response = table.delete_item(
+            Key=key
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': f"Player {player_name} deleted successfully for season {season}."
+            })
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': f"Error deleting player {player_name}: {str(e)}"
+            })
+        }
